647b4b3878be8e0d2480d26a2ecc3660e00972a0
rongqiao.yurq <rongqiao.yurq@taobao.com>
2018-07-01 15:42:45 +0800

    udpate.

--- playground/django/mysite2/blog/admin.py
+++ playground/django/mysite2/blog/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import BlogArticle
 
 # Register your models here.
+
+
+class BlogArticleAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'publish']
+    list_filter = ['publish', 'author']
+    search_fields = ['title', 'body']
+    date_hierarchy = 'publish'
+    ordering = ('publish', 'author')
+
+admin.site.register(BlogArticle, BlogArticleAdmin)
--- playground/django/mysite2/blog/views.py
+++ playground/django/mysite2/blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import BlogArticle
 
 # Create your views here.
+
+def blog_title(request):
+    blogs = BlogArticle.objects.all()
+    return render(request, 'blog/titles.html', dict(blogs=blogs))
